19ac75cdde0b165689ea5e963653fb7884a3b61c
Nicole D <nnardella@gmail.com>
2018-11-09 17:22:41 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+require 'pry'
+
+class Song
+
+  attr_accessor
+  attr_reader :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+   @name = name
+   @artist = artist
+   @genre = genre
+
+   @@count += 1
+   @@genres << @genre
+   @@artists << @artist
+
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.artists
+      @@artists.uniq!
+  end
+
+  def self.genre_count
+
+  
+
+    @@genres= Hash.new(0)
+
+    @@genres.collect do |key, value|
+      if @@genres.include?(key)
+           @@genres[value] += 1
+      else
+        @@genres[value] = [key]
+      end
+    end
+  end
+
+  def self.artist_count
+
+  end
+
+end
